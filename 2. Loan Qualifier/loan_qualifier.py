f4d11424560e5a91737fe03de5ba20587e8e1da9
Vishwjeet Singh Vilkhu <136313274+chill-vishu@users.noreply.github.com>
2024-08-27 15:59:27 +0530

    Update loan_qualifier.py

--- 2. Loan Qualifier/loan_qualifier.py
+++ 2. Loan Qualifier/loan_qualifier.py
@@ -1,8 +1,10 @@
+# Program to determine whether a bank customer qualifies for a loan.
+
 def loan_qualifier(income, credit_score, debt, loan_amount):
     if income > 50000 and credit_score > 700 and debt < loan_amount * 0.4:
         return "You qualify for the loan."
     else:
         return "You do not qualify for the loan."
 
-# Example usage
+# Example For Usage
 print(loan_qualifier(60000, 720, 10000, 200000))
