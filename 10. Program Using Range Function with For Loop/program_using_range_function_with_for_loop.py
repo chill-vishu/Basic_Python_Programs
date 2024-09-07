1639fdb19dcbdf933c174c582581eab9cd98893c
Vishwjeet Singh Vilkhu <136313274+chill-vishu@users.noreply.github.com>
2024-09-07 21:22:07 +0530

    Uploaded 10th Program

---
+++ 10. Program Using Range Function with For Loop/program_using_range_function_with_for_loop.py
@@ -0,0 +1,8 @@
+# Write A Program That Demonstrates How The Range Function Can Be Used With A For Loop.
+
+def range_with_for_loop():
+    for i in range(1, 11):
+        print(i)
+
+# Call the function to execute
+range_with_for_loop()
