9f4fb614e77a8760a570f956533e51e17ad0d4a2
Vishwjeet Singh Vilkhu <136313274+chill-vishu@users.noreply.github.com>
2024-09-09 18:47:08 +0530

    Uploaded 11th Program

---
+++ 11. Print Asterisks in a Triangle Pattern/print_asterisks_in_a_triangle_pattern.py
@@ -0,0 +1,9 @@
+# Create A Program That Print Asterisks In A Pattern That Looks Like The Triangle.
+
+def print_triangle():
+    rows = 5
+    for i in range(1, rows + 1):
+        print('*' * i)
+
+# Calling Function
+print_triangle()
